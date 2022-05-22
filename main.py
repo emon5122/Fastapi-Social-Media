d7953cf6801bc43ee18f2db1467791b70af0b351
Istiak Hassan Emon <emon5122@Istiaks-iMac.local>
2022-05-22 16:32:12 +0600

    Init

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import AsyncIterable
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Does it work?"}
+
+@app.get("/app")
+def jhsad():
+    return{'title': 'it works'}
