588248301e465bd573ef7ea647f063429755b238
Istiak Hassan Emon <emon5122@gmail.com>
2022-05-23 04:01:34 +0600

    A lot

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
 from turtle import title
 from typing import AsyncIterable, Optional
+from urllib.request import CacheFTPHandler
 from fastapi import FastAPI,status
 from fastapi.params import Body
 from pydantic import BaseModel
 from http import HTTPStatus
 import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
-
+# Initializes FastAPI
 app = FastAPI()
+#Database Connection
+while True:
+    try:
+        # Connect to your postgres DB
+        conn = psycopg2.connect(host="database", database="FastAPI" ,user="postgres",password="sdkfhkj234ou23o4ldfsdn", cursor_factory=RealDictCursor)
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
+        print("Connection to the database sucessful.")
+        break
+    except Exception as error:
+        print('Failed', error)
+        time.sleep(5)
 
 class post(BaseModel):
     title: str
@@ -15,10 +30,19 @@ class post(BaseModel):
     isPublished: bool = True
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Oh yeah"}
 
-@app.post("/", status_code=status.HTTP_201_CREATED)
-def createpost(rec_post: post):
-    rec_post.dict()
-    return {"message":rec_post}
+@app.get("/posts")
+def get_posts():
+    cur.execute("""SELECT * FROM posts """)
+    posts = cur.fetchall()
+    return {"Response" :posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: post):
+    cur.execute("""INSERT INTO posts (title, content, isPublished) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.isPublished))
+    new_post = cur.fetchone()
+    conn.commit()
+    return {"message":new_post}
