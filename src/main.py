0ee8327e6d92ac1bc52e324085f0fded6d2e6950
Istiak Hassan Emon <emon5122@gmail.com>
2022-05-22 21:42:31 +0600

    status code

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
+from turtle import title
 from typing import AsyncIterable
-from fastapi import FastAPI
+from fastapi import FastAPI,status
+from fastapi.params import Body
+from pydantic import BaseModel
+from http import HTTPStatus
 
 app = FastAPI()
 
+class post(BaseModel):
+    title: str
+    content: str
+    isPublished: bool = True
 
 @app.get("/")
 async def root():
-    return {"message": "Does it work?"}
+    return {"message": "Oh yeah"}
 
-@app.get("/app")
-def jhsad():
-    return{'title': 'it works'}
-
-@app.get('/test')
-def test():
-    return {"message": "try"}
+@app.post("/", status_code=status.HTTP_201_CREATED)
+def createpost(rec_post: post):
+    rec_post.dict()
+    return {"message":rec_post}
